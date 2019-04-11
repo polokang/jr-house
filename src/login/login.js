1bbd008440d5674376dfdf7ea84568d78310efce
wzht1991 <506486189@qq.com>
2019-04-12 00:40:19 +1000

    login _ul

---
+++ src/login/login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Input, Icon, Button} from 'antd';
+export default class Login extends React.Component{
+    // 整个位置想弄一个function onsubmit 调用 如果用户名 密码正确 就去一个页面  否则跳回 login 页面<login /> 这样子会把它放在类似weatherchannel.js中
+    render(){
+    return(
+  
+  <div className="login_page">
+  <h1>Login</h1>
+  <form>
+  <Input
+        placeholder="Enter your username"
+        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)'}} />}
+     />
+     <Input.Password placeholder="input password" />
+  <div className="button">
+   <Button type="primary" htmlType="submit" className="login-button" >
+   Log in
+   </Button> 
+   <Button type="primary" htmlType="submit" className="register-button">
+   Register
+   </Button> 
+   </div >
+  </form>
+ </div>
+    )
+ }
+
+}
